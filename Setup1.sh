a28395041f90e7feebc0914ba77a77a798129be9
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-04-18 06:44:18 +0800

    Update Setup1.sh

--- Setup1.sh
+++ Setup1.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
-echo -e "MASUKKAN TOKEN BOT"
-read -p "Masukkan Token :" token_bot
-
-# Update paket repository
-sudo apt update -y
-
-# Upgrade paket yang sudah terinstal
-sudo apt upgrade -y
-
-# Instal Python 3 dan pip
-sudo apt install python3 python3-pip -y
-
-# Instal dependensi Python
-pip install requests
-pip install python-telegram-bot==12.0.0
-
 mkdir -p grub/bot
 # Pindah ke dalam folder yang baru dibuat
 cd grub/bot
 # Mengunduh skrip Python
 wget https://raw.githubusercontent.com/Paper890/Grubbot/main/Botgrb.py
 chmod +x Botgrb.py
-# FOR DO CREATE
-TOKEN="$token_bot"
-sed -i "s/{TOKEN}/$TOKEN/g" Botgrb.py
-
+# 
 # fungsi running as system
 cd
 cd /etc/systemd/system
@@ -34,7 +15,6 @@ sudo systemctl daemon-reload
 sudo systemctl start Botgrb
 sudo systemctl enable Botgrb
 sudo systemctl restart Botgrb
-
 
 echo "Instalasi selesai."
 
